446028d4580654642f6955eb92347c40935b680f
AntonioViscomi <antonio.viscomi@iit.it>
2025-11-27 16:22:17 +0100

    Assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -17,7 +17,7 @@ bool do_system(const char *cmd)
  *   or false() if it returned a failure
 */
 
-    return true;
+    return system(cmd) == 0;
 }
 
 /**
@@ -58,7 +58,16 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int status;
+    pid_t pid = fork();
+    if (pid == -1) { return false; } 
+    else if (pid == 0) {
+        execv(command[0], command);
+        exit(1); 
+    }
 
+    if (waitpid(pid, &status, 0) == -1) { return false; }
+    else if (WIFEXITED(status)){ return WEXITSTATUS(status) == 0; }
     va_end(args);
 
     return true;
@@ -92,6 +101,25 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) { return false; }
+
+    int status;
+    pid_t pid = fork();
+    if (pid == -1) { close(fd); return false; }
+    else if (pid == 0) {
+        if (dup2(fd, 1) < 0) { exit(1); }
+        close(fd);
+        execv(command[0], command);
+        exit(1); 
+    }
+    
+    close(fd);  
+
+    if (waitpid(pid, &status, 0) == -1) {
+        return false;
+    }
+    else if (WIFEXITED(status)){ return WEXITSTATUS(status) == 0; }
 
     va_end(args);
 
--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -2,6 +2,17 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
+#include <stdlib.h>
+
+#include<unistd.h>
+
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+
+#include <fcntl.h>
+
 bool do_system(const char *command);
 
 bool do_exec(int count, ...);
