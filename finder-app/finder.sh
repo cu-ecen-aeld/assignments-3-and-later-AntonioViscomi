0474d6d55076c183769a783ab37d6caa71f9d404
AntonioViscomi <antonio.viscomi@iit.it>
2025-12-05 12:30:54 +0100

    Assignment 3 part 2

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 FILESDIR=$1
 SEARCHSTR=$2
-args_num=$#
 
 if [ $# -ne 2 ]
 then
-	echo "Missing arguments. Exit with value 1."
-	exit 1
+    echo "Missing arguments. Exit with value 1."
+    exit 1
 else
-	if [ -d ${FILESDIR} ]
-	then
-		X=$(find "$FILESDIR" -type f | wc -l)
-		Y=$(grep -r "$SEARCHSTR" "$FILESDIR"| wc -l)
-		echo "The number of files are ${X} and the number of matching lines are ${Y}"
-	else
-		echo "${FILESDIR} is not a directory."
-		exit 1
-	fi
-
+    # Quote the variable "$FILESDIR" to handle spaces safely in /bin/sh
+    if [ -d "$FILESDIR" ]
+    then
+        X=$(find "$FILESDIR" -type f | wc -l)
+        Y=$(grep -r "$SEARCHSTR" "$FILESDIR" | wc -l)
+        echo "The number of files are ${X} and the number of matching lines are ${Y}"
+    else
+        echo "${FILESDIR} is not a directory."
+        exit 1
+    fi
 fi
 
 exit 0
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,14 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make -j20 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make -j20 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j20 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j20 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" "${OUTDIR}/"
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +53,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+sudo mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+sudo mkdir -p usr/bin usr/lib usr/sbin
+sudo mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +66,58 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make -j20  distclean
+    make -j20  defconfig
 else
     cd busybox
+    make -j20  distclean
+    make -j20  defconfig
 fi
 
 # TODO: Make and install busybox
+make -j20 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+sudo env "PATH=$PATH" make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
+
+cd "${OUTDIR}/rootfs"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+sudo cp "${SYSROOT}/lib/ld-linux-aarch64.so.1" "${OUTDIR}/rootfs/lib/"
+sudo cp "${SYSROOT}/lib64/libm.so.6" "${OUTDIR}/rootfs/lib64/"
+sudo cp "${SYSROOT}/lib64/libresolv.so.2" "${OUTDIR}/rootfs/lib64/"
+sudo cp "${SYSROOT}/lib64/libc.so.6" "${OUTDIR}/rootfs/lib64/"
 
 # TODO: Make device nodes
+cd ${OUTDIR}/rootfs
+
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd "${FINDER_APP_DIR}"
+make clean
+make -j20 CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+sudo cp "${FINDER_APP_DIR}/finder.sh" "${OUTDIR}/rootfs/home/"
+sudo cp "${FINDER_APP_DIR}/finder-test.sh" "${OUTDIR}/rootfs/home/"
+sudo cp "${FINDER_APP_DIR}/autorun-qemu.sh" "${OUTDIR}/rootfs/home/"
+sudo cp "${FINDER_APP_DIR}/writer" "${OUTDIR}/rootfs/home/"
+sudo mkdir -p "${OUTDIR}/rootfs/home/conf"
+sudo cp "${FINDER_APP_DIR}/conf/assignment.txt" "${OUTDIR}/rootfs/home/conf/"
+sudo cp "${FINDER_APP_DIR}/conf/username.txt" "${OUTDIR}/rootfs/home/conf/"
 
 # TODO: Chown the root directory
+cd "${OUTDIR}/rootfs"
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+cd "${OUTDIR}/rootfs"
+sudo find . | cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
+gzip -f "${OUTDIR}/initramfs.cpio"
